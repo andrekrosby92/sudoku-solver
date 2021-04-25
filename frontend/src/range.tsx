25a1047310797ea2b47412af588f54888d648830
André Krosby <andre.krosby92@icloud.com>
2021-04-25 20:03:28 +0200

    Added documentation for SudokuService and range-function.

--- frontend/src/range.tsx
+++ frontend/src/range.tsx
@@ -1,4 +1,12 @@
-export function range(start: number, stop?: number, step?: number) {
+/**
+ * Helper-function for loops, similar to that of Pythons range().
+ * If only one argument is passed, {start} is 0, {step} is 1 and {stop} is the argument.
+ * @param {number} start An integer number.
+ * @param {number} [stop] An integer number.
+ * @param {number} [step] An integer number specifying the progression.
+ * @return {Array} A list of arithmetic progressions.
+ */
+export function range(start: number, stop?: number, step?: number): Array<number> {
   if (typeof stop == 'undefined') {
     stop = start;
     start = 0;
